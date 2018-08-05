54473f48acfde1f665e84667e36e56f60a89fc81
Jason Huey <jason.huey1@gmail.com>
2018-08-05 16:04:43 -0700

    update

--- js/Main.js
+++ js/Main.js
@@ -1,1 +1,10 @@
-console.log("connected");
+// function for menu 
+
+$("#menu-button").click(function() {
+	$("#menu-button").toggleClass("menu-open");
+	$("#menu").toggleClass("menu-collapsed");
+	$("#menu ul li").toggleClass("hidden animated fadeInLeft");
+});
+
+
+
